c53bbcb0f8af93453d6d7095ec76bc67d617e541
Tammy Metz <tammy@tammymetz.com>
2018-07-15 17:57:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  end
   else
     output_string =  "The line is currently:"
     katz_deli.each_with_index do |person, index|
