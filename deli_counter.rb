48c5df4615ef24a9c529ccad34006f4ff747f2cc
Tammy Metz <tammy@tammymetz.com>
2018-07-15 18:00:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}"
 end
 
 katz_deli = []
